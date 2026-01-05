23e8eca27f0e07353a8da171905bedd43bc0e697
Gowtham Kishore <techigowtham@gmail.com>
2026-01-05 18:21:21 +0530

    start to work in front end first im start in header

--- frontend/src/components/shopingView/header.jsx
+++ frontend/src/components/shopingView/header.jsx
@@ -1,8 +1,28 @@
+import { HousePlug } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Sheet, SheetTrigger } from '../ui/sheet'
+import { Button } from '../ui/button'
 
 function Shoppingheader (){
   return (
-    <div>Shppoing Header</div>
+    <>
+      <header className='sticky top-0 z-40 w-full border-b bg-background'>
+        <div className='flex h-16 items-center justify-between px-4 md:px-6'>
+          <Link className='flex items-center gap-2' to='/shop/home'>
+          <HousePlug className='w-6 h-6'/>
+          <span className='font-bold'>Ecommerce</span>
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant='outline' size='icon' className='lg:hidden'>
+                <span className='sr-only'>Toggle header menu</span>
+              </Button>
+            </SheetTrigger>
+          </Sheet>
+        </div>
+      </header>
+    </>
   )
 }
 
